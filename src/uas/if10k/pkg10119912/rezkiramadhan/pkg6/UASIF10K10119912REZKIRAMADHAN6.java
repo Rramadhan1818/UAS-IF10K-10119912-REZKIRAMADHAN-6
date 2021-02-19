11515b0436c42e4f3b95ba72e7fabe5fd6741eb3
Rezki Ramadhan <rezki@dev.netkromsolution.com>
2021-02-19 20:35:50 +0700

    first commit

---
+++ src/uas/if10k/pkg10119912/rezkiramadhan/pkg6/UASIF10K10119912REZKIRAMADHAN6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119912.rezkiramadhan.pkg6;
+
+/**
+ *
+ * @author ENG-DEV23
+ */
+public class UASIF10K10119912REZKIRAMADHAN6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
